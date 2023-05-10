dbc01d6b7097554c8846d12c87c2d397ce880a3e
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-05-10 15:45:14 +0300

    feat: done 60% of HW3

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -1,66 +1,82 @@
-import React, { ChangeEvent, KeyboardEvent, useState } from 'react'
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react'
 import Greeting from './Greeting'
-import { UserType } from './HW3'
+import {UserType} from './HW3'
+import {nativeSelectClasses} from "@mui/material";
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+  users: UserType[]
+  addUserCallback: (name: string) => void
 }
 
-export const pureAddUser = (name: any, setError: any, setName: any, addUserCallback: any) => {
-    // если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут
+export const pureAddUser = (
+  name: string,
+  setError: React.Dispatch<React.SetStateAction<string>>,
+  setName: React.Dispatch<React.SetStateAction<string>>,
+  addUserCallback: (name: string) => void) => {
+  // если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут
+  if (name.trim() === '') {
+    setError("Ошибка! Введите имя!")
+  } else {
+    addUserCallback(name)
+    setName('')
+    setError('')
+  }
 }
 
-export const pureOnBlur = (name: any, setError: any) => { // если имя пустое - показать ошибку
+export const pureOnBlur = (name: string, setError: React.Dispatch<React.SetStateAction<string>>) => {
+  // если имя пустое - показать ошибку
+  if (name.trim() === '') {
+    setError("Ошибка! Введите имя!")
+  }
 }
 
-export const pureOnEnter = (e: any, addUser: any) => { // если нажата кнопка Enter - добавить
+export const pureOnEnter = (event: KeyboardEvent<HTMLInputElement>, addUser: () => void) => {
+  if (event.key === 'Enter') {
+    addUser()
+  }// если нажата кнопка Enter - добавить
 }
 
 // более простой и понятный для новичков
 // function GreetingContainer(props: GreetingPropsType) {
 
 // более современный и удобный для про :)
-const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
-    users,
-    addUserCallback,
-}) => {
-    // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+// деструктуризация пропсов
+const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback,}) => {
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('some name') // need to fix
+  const [name, setName] = useState<string>('')
+  const [error, setError] = useState<string>('')
 
-        error && setError('')
-    }
-    const addUser = () => {
-        pureAddUser(name, setError, setName, addUserCallback)
-    }
+  const setNameCallback = (name: string) => {
+    setName(name)
+    error && setError('')
+  }
+  const addUser = () => {
+    pureAddUser(name, setError, setName, addUserCallback)
+  }
 
-    const onBlur = () => {
-        pureOnBlur(name, setError)
-    }
+  const onBlur = () => {
+    pureOnBlur(name, setError)
+  }
 
-    const onEnter = (e: any) => {
-        pureOnEnter(e, addUser)
-    }
+  const onEnter = (e: any) => {
+    pureOnEnter(e, addUser)
+  }
 
-    const totalUsers = 0 // need to fix
-    const lastUserName = 'some name' // need to fix
+  const totalUsers = 0 // need to fix
+  const lastUserName = 'some name' // need to fix
 
-    return (
-        <Greeting
-            name={name}
-            setNameCallback={setNameCallback}
-            addUser={addUser}
-            onBlur={onBlur}
-            onEnter={onEnter}
-            error={error}
-            totalUsers={totalUsers}
-            lastUserName={lastUserName}
-        />
-    )
+  return (
+    <Greeting
+      name={name}
+      setNameCallback={setNameCallback}
+      addUser={addUser}
+      onBlur={onBlur}
+      onEnter={onEnter}
+      error={error}
+      totalUsers={totalUsers}
+      lastUserName={lastUserName}
+    />
+  )
 }
 
 export default GreetingContainer
--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { v1 } from 'uuid'
+import React, {useState} from 'react'
+import {v1} from 'uuid'
 import s2 from '../../s1-main/App.module.css'
 import GreetingContainer from './GreetingContainer'
 
@@ -7,7 +7,7 @@ import GreetingContainer from './GreetingContainer'
 * 1 - описать тип UserType +
 * 2 - указать нужный тип в useState с users +
 * 3 - дописать типы и логику функции pureAddUserCallback и проверить её тестами +
-* 4 - в файле GreetingContainer.tsx дописать типизацию пропсов
+* 4 - в файле GreetingContainer.tsx дописать типизацию пропсов +
 * 5 - в файле GreetingContainer.tsx указать нужные типы в useState с name и error
 * 6 - в файле GreetingContainer.tsx дописать тип и логику функции setNameCallback
 * 7 - в файле GreetingContainer.tsx дописать логику функций pureAddUser, pureOnBlur, pureOnEnter и проверить их тестами
@@ -19,41 +19,41 @@ import GreetingContainer from './GreetingContainer'
 
 // types
 export type UserType = {
-    _id: string // need to fix any
-    name: string // need to fix any
+  _id: string // need to fix any
+  name: string // need to fix any
 }
 
 export const pureAddUserCallback = (
   name: string,
   setUsers: React.Dispatch<React.SetStateAction<UserType[]>>, // тип, когда функция set в качестве аргумента принимает
   users: UserType[]) => {                                     // новое значение состояния state
-    const user = {
-        _id: v1(),
-        name: name
-    }
-    setUsers([...users, user])
+  const user = {
+    _id: v1(),
+    name: name
+  }
+  setUsers([...users, user])
 }
 
 const HW3 = () => {
-    const [users, setUsers] = useState<UserType[]>([])
+  const [users, setUsers] = useState<UserType[]>([])
 
-    const addUserCallback = (name: string) => {
-        pureAddUserCallback(name, setUsers, users)
-    }
+  const addUserCallback = (name: string) => {
+    pureAddUserCallback(name, setUsers, users)
+  }
 
-    return (
-        <div id={'hw3'}>
-            <div className={s2.hwTitle}>Homework #3</div>
-            {/*для автоматической проверки дз (не менять)*/}
+  return (
+    <div id={'hw3'}>
+      <div className={s2.hwTitle}>Homework #3</div>
+      {/*для автоматической проверки дз (не менять)*/}
 
-            <div className={s2.hw}>
-                <GreetingContainer
-                    users={users}
-                    addUserCallback={addUserCallback}
-                />
-            </div>
-        </div>
-    )
+      <div className={s2.hw}>
+        <GreetingContainer
+          users={users}
+          addUserCallback={addUserCallback}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default HW3
